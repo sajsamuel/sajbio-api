d9f87e5fd9a8c701af5776ad1a83adaf8740b6b4
sajsamuel <saj.kolathu@gmail.com>
2026-02-16 18:05:00 -0500

    Refactor main function to use FunctionApp routing

--- sajbio/__init__.py
+++ sajbio/__init__.py
@@ -1,12 +1,10 @@
-import json
 import azure.functions as func
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    bio = {
-        "name": "Saj Samuel",
-        "title": "Data Engineer / SQL Developer",
-        "location": "New Hyde Park, NY",
-        "skills": ["Azure", "SQL Server", "Python", "ETL"],
-        "current_focus": "Building Azure Serverless Applications"
-    }
-    return func.HttpResponse(json.dumps(bio), mimetype="application/json")
+app = func.FunctionApp()
+
+@app.route(route="sajbio", auth_level=func.AuthLevel.ANONYMOUS)
+def sajbio(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        '{"name":"Saj Samuel","title":"Azure Builder"}',
+        mimetype="application/json"
+    )
